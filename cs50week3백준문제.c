63378d9664bf44bf3e74a9e836608d2ff6ff7697
dksxogus68-design <dksxogus68@gmail.com>
2026-04-03 14:46:03 +0900

    Add files via upload
    cs50/week3/문제풀이

---
+++ cs50week3백준문제.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    char str[101];
+    int alpha[26];
+    
+    for(int i = 0; i < 26; i++) {
+        alpha[i] = -1;
+    }
+    scanf("%s", str);
+    
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(alpha[str[i] - 'a'] == -1) {
+            alpha[str[i] - 'a'] = i;
+        }
+    }
+    for(int i = 0; i < 26; i++) {
+        printf("%d ", alpha[i]);
+    }
+    return 0;
+}
